43330338730efaafee6aca5e328b3b0766ee7cef
Magnus Karström <magnus@yesweb.se>
2022-12-27 14:48:06 +0100

    init with tailwind installed

---
+++ src/components/Header.svelte
@@ -0,0 +1,5 @@
+<div class="container flex flex-row items-center justify-between h-12 py-0 px-4 bg-[var(--yt-dark)]">
+    <div id="start"></div>
+    <div id="center"></div>
+    <div id="end"></div>
+</div>
