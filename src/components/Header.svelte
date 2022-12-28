26873a7266d4f753eea07ed9fd068882c1af3ae0
Magnus Karström <magnus@yesweb.se>
2022-12-28 08:56:54 +0100

    fix (app.html): Added link to fontawesome

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,12 @@
-<div class="container flex flex-row items-center justify-between h-12 py-0 px-4 bg-[var(--yt-dark)]">
-    <div id="start"></div>
-    <div id="center"></div>
-    <div id="end"></div>
+<div
+	class="wrapper flex flex-col items-center justify-between w-full h-12 py-0 px-4 bg-[var(--yt-dark)]"
+>
+	<div id="start" class="flex flex-row items-center">
+		<div class="menu-btn w-10 h-10 p-2">
+			<i class="fa-sharp fa-solid fa-bars text-white" />
+		</div>
+		<div class="yt-logo" />
+	</div>
+	<div id="center" />
+	<div id="end" />
 </div>
